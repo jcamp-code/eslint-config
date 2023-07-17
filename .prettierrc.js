043dfd4d6f012af09784b02473268ba149253e3b
John Campion Jr <john@brightshore.com>
2023-07-16 21:12:28 -0400

    fix: verify new build process works

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,13 @@
-const baseConfig = require('@jcamp/eslint-config/.prettierrc.js')
-
+// prettier.config.js
+// some of these match the current Prettier defaults, but are left in for clarity
 module.exports = {
-  ...baseConfig,
-  /* make any changes here */
+  arrowParens: 'always',
+  bracketSameLine: false,
+  endOfLine: 'lf',
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  singleAttributePerLine: false,
+  plugins: ['prettier-plugin-tailwindcss'],
 }
