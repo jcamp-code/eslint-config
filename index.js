331bd04c28d1208f8f651a7262830a97444eb966
John Campion Jr <john@brightshore.com>
2023-10-09 17:54:26 -0400

    fix: disable eslint/sort-imports
    Doesn't like types sorted after non-types
    
    chore: update antfu to 1.0.0.beta24

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ config.push(eslintConfigPrettier)
 
 config.push({
   rules: {
+    // this doesn't like the ordering of types at the end of the imports
+    'sort-imports': ['off'],
+
+    // Prettier can handle this setting, see https://eslint.org/docs/latest/rules/curly#consistent
     curly: ['error', 'multi', 'consistent'],
+
+    // required config for Prettier https://github.com/vuejs/eslint-plugin-vue/blob/master/docs/rules/html-self-closing.md
     'vue/html-self-closing': [
       'error',
       {
