2d8212a5dd45d4febcf8e9ceb90ae290de4270a8
John Campion Jr <john@brightshore.com>
2023-10-09 22:28:46 -0400

    ci: fix ignores

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,15 @@
 import config from './index.js'
 
+config.push({
+  ignores: ['dist',
+    'pnpm-lock.yaml',
+    'cache',
+    'temp',
+    'fixtures',
+    '.github',
+    'node_modules',
+    '.pnpm-store'
+    ]
+})
+
 export default config
